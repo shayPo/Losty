46e03bb402e3eb82590e708acfeb8d06c3db094b
Shay Poluck <shay@opentech.co.il>
2018-05-30 12:14:52 +0300

    backup search screen list adapter

---
+++ app/src/main/java/com/fond/lost/losty/view/adapters/SearchResults.kt
@@ -0,0 +1,48 @@
+package com.fond.lost.losty.view.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fond.lost.losty.R
+import com.fond.lost.losty.model.SearchItem
+import kotlinx.android.synthetic.main.search_item.view.*
+
+/**
+ * Created by Sahar on 28/05/2018.
+ */
+class SearchResults : RecyclerView.Adapter<SearchResults.Holder>
+{
+    var mItems : ArrayList<SearchItem> = ArrayList<SearchItem>()
+
+    constructor(data : List<SearchItem>)
+    {
+        mItems = ArrayList()
+        mItems.addAll(data)
+    }
+
+
+    override fun getItemCount(): Int {
+        return mItems.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.search_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+         holder.mDistance.text = "2.5 ק״מ"
+        holder.mDescription.text = "two"
+        holder.mLocation.text = "three"
+        holder.mItemImage.setImageResource(R.drawable.type_dog)
+    }
+
+    class Holder(view: View) : RecyclerView.ViewHolder(view)
+    {
+        public val mDistance = view.distance
+        public val mDescription = view.description
+        public val mLocation = view.location
+        public val mItemImage = view.item_image
+    }
+}
--- app/src/main/java/com/fond/lost/losty/view/fragments/AdvenceSearch.kt
+++ app/src/main/java/com/fond/lost/losty/view/fragments/AdvenceSearch.kt
@@ -4,14 +4,16 @@ import android.app.AlertDialog
 import android.app.Fragment
 import android.content.DialogInterface
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.fond.lost.losty.R
+import com.fond.lost.losty.model.SearchItem
+import com.fond.lost.losty.view.adapters.SearchResults
 import kotlinx.android.synthetic.main.advench_search.*
-
 
 
 /**
@@ -19,10 +21,10 @@ import kotlinx.android.synthetic.main.advench_search.*
  */
 class AdvenceSearch : Fragment(), View.OnClickListener, DialogInterface.OnClickListener {
 
-    var mType : Int = 0
-    var mDialogItems : Array<String>? = null
-    var mButton : TextView? = null
-    var mNumberOfResulte : Int = 0;
+    var mType: Int = 0
+    var mDialogItems: Array<String>? = null
+    var mButton: TextView? = null
+    var mNumberOfResulte: Int = 0;
 
     override fun onCreateView(inflater: LayoutInflater?,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -34,14 +36,13 @@ class AdvenceSearch : Fragment(), View.OnClickListener, DialogInterface.OnClickL
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if(view != null) {
+        if (view != null) {
             initView()
         }
     }
 
-    private fun initView()
-    {
-        if(mType == TYPE_ADVENCH_SEARCH) {
+    private fun initView() {
+        if (mType == TYPE_ADVENCH_SEARCH) {
             title_type.setText(R.string.select_object)
             selected_type.setText(R.string.item)
             selected_type.setOnClickListener(this)
@@ -49,9 +50,7 @@ class AdvenceSearch : Fragment(), View.OnClickListener, DialogInterface.OnClickL
             title_city.setText(R.string.select_city)
             selected_city.setText(R.string.city)
             selected_city.setOnClickListener(this)
-        }
-        else if(mType == TYPE_ADVENCH_SEARCH_PUBLIC_TRANSPORTATION)
-        {
+        } else if (mType == TYPE_ADVENCH_SEARCH_PUBLIC_TRANSPORTATION) {
             title_type.setText(R.string.select_transportin_type)
             selected_type.setText(R.string.transportin_type)
             selected_type.setOnClickListener(this)
@@ -63,31 +62,27 @@ class AdvenceSearch : Fragment(), View.OnClickListener, DialogInterface.OnClickL
     }
 
     override fun onClick(v: View?) {
-         if(v!!.id == R.id.selected_city)
-         {
-             mButton = v as TextView?
-             createDialog(R.array.cities, getString(R.string.select_city))
-         }
-          else if(v!!.id == R.id.selected_type)
-         {
-             mButton = v as TextView?
-             createDialog(R.array.types, getString(R.string.select_object))
-         }
+        mButton = v as TextView?
+        if (v!!.id == R.id.selected_city) {
+            createDialog(R.array.cities, getString(R.string.select_city))
+        } else if (v!!.id == R.id.selected_type) {
+            createDialog(R.array.types, getString(R.string.select_object))
+        }
     }
 
-    private fun createDialog(items: Int, title : String) {
+    private fun createDialog(items: Int, title: String) {
         val builderSingle = AlertDialog.Builder(activity)
         builderSingle.setTitle(title)
         mDialogItems = resources.getStringArray(items)
-        var adapter : ArrayAdapter<String> = ArrayAdapter(activity, R.layout.display_item, mDialogItems)
+        var adapter: ArrayAdapter<String> = ArrayAdapter(activity, R.layout.display_item, mDialogItems)
         builderSingle.setAdapter(adapter, this)
         builderSingle.show()
     }
 
     override fun onClick(p0: DialogInterface?, p1: Int) {
-        if(mButton != null) {
-            if(mButton!!.text.equals(""))
-            {
+        if (mButton != null) {
+            if (mButton!!.tag != null) {
+                mButton!!.tag = null
                 mNumberOfResulte++
             }
             mButton!!.setText(mDialogItems!![p1])
@@ -97,28 +92,45 @@ class AdvenceSearch : Fragment(), View.OnClickListener, DialogInterface.OnClickL
                 p0.dismiss()
             }
         }
-        if(mNumberOfResulte > 1)
-        {
-            type.text = selected_type.text
-            city.text = selected_city.text
-            sprator.visibility = View.VISIBLE
-            number_of_results.text = "12 תוצאות"
-
+        if (mNumberOfResulte > 1) {
+            var searchPramters = StringBuilder()
+            searchPramters.append(selected_type.text)
+            searchPramters.append(" | ")
+            searchPramters.append(selected_city.text)
+            search_pramters.text = searchPramters.toString()
+            temp()
         }
+    }
+
+    private fun temp() {
+        var results = ArrayList<SearchItem>()
+        results.add(SearchItem(""))
+        results.add(SearchItem(""))
+        results.add(SearchItem(""))
+        results.add(SearchItem(""))
+        results.add(SearchItem(""))
+        setupresults(results)
+    }
+
+    private fun setupresults(results: ArrayList<SearchItem>)
+    {
+        number_of_results.text = ""+ results.size + " תוצאות "
+        results_display.layoutManager = GridLayoutManager(activity, 1)
+        results_display.adapter = SearchResults(results)
     }
 
     companion object {
 
-    val TYPE_ADVENCH_SEARCH : Int = 1
-    val TYPE_ADVENCH_SEARCH_PUBLIC_TRANSPORTATION : Int = 2
-    val TYPE : String = "type"
+        val TYPE_ADVENCH_SEARCH: Int = 1
+        val TYPE_ADVENCH_SEARCH_PUBLIC_TRANSPORTATION: Int = 2
+        val TYPE: String = "type"
 
-    fun newInstance(type : Int): Fragment {
-        val args: Bundle = Bundle()
-        args.putInt(TYPE, type)
-        val fragment = AdvenceSearch()
-        fragment.arguments = args
-        return fragment
+        fun newInstance(type: Int): Fragment {
+            val args: Bundle = Bundle()
+            args.putInt(TYPE, type)
+            val fragment = AdvenceSearch()
+            fragment.arguments = args
+            return fragment
+        }
     }
-}
 }
